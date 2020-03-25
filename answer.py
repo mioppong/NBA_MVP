99441cb15243ad3aab55f6ccf8786574b2aa1e03
oppong <michaeloppong123@gmail.com>
2020-03-25 16:26:50 -0400

    so answer.py now gets players within current nba season within my 0.2-0.35 value

--- answer.py
+++ answer.py
@@ -20,13 +20,28 @@ df_players = pd.DataFrame()
 for x in client.players_advanced_season_totals(season_end_year=current_year):
     df_players = df_players.append(pd.DataFrame(x), ignore_index=True)
 
+df_players = df_players.sort_values(by='win_shares', ascending=False, ignore_index=True).head(50)
 df_players = df_players[['slug','name','win_shares']]
 df_players['my_calc'] = ""
-df_players['wins']
+df_players['wins'] = 0
 
 for index, player in df_players.iterrows():
-    DataFrame(client.regular_season_player_box_scores(player_identifier=player['slug'],season_end_year=current_year))
-    
-    player['wins']
+    player_stats = pd.DataFrame(client.regular_season_player_box_scores(player_identifier=player['slug'],season_end_year=current_year))
+    counter = 0
+    for x in player_stats['outcome']:
+        if (x.value) == 'WIN':
+            counter += 1
+    df_players.loc[index,'wins'] = counter
+    df_players.loc[index,'my_calc'] = round(player['win_shares']/counter,2)
+        
 
-print(df_players)
+#---------- HERE I REMVOE PLAYERS NOT WITHIN MY VALUE OF 0.2 TO 0.35
+df_players = df_players[df_players.my_calc > 0.20]
+df_players = df_players[df_players.my_calc < 0.35]
+
+
+with open("prediction.txt", 'w+') as f:
+    for index,player in df_players.iterrows():
+        f.write(player['name']+ '\n')
+
+#print(df_players)
