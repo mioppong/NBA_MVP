c4b2557453395997599b8748185dac71efeceb69
oppong <michaeloppong123@gmail.com>
2020-03-24 23:33:59 -0400

    IT WORKS, SO NOW I CAN PLOT FROM JUST MVPS.PY

--- past_mvp/mvps.py
+++ past_mvp/mvps.py
@@ -72,8 +72,6 @@ for index, player in df.iterrows():
     for index2, player2 in df_temp.iterrows():
         if player2['outcome'].value == 'WIN':
             win_counter = win_counter + 1
-
-    #print(player['Player'], ' supposed to have win ->', win_counter)
     player['Wins'] = win_counter
 
 df['WS']  =df['WS'].astype(float)
@@ -86,14 +84,11 @@ df['my_calc'] = round(df['WS'] / df['Wins'],2)
 
 ##--------------\\\GETTING REGULAR SEASON PLAYERS AND THEIR WINSHARES
 
-
-
-
 df_season = pd.DataFrame()
-for x in (client.players_advanced_season_totals(season_end_year=2020)):
+for x in (client.players_advanced_season_totals(season_end_year=current_year)):
     df_season = df_season.append(pd.DataFrame(x), ignore_index=True)
 
-df_season = df_season.head(100)
+df_season = df_season.sort_values(by='win_shares', ascending=False).head(100)
 df_season['Wins'] = ""
 
 df_regular_players = pd.DataFrame()
@@ -101,23 +96,30 @@ df_regular_players['name'] = ""
 df_regular_players['slug'] = ""
 df_regular_players['wins'] = ""
 df_regular_players['WS'] = ""
+df_regular_players['my_calc'] =0
 
 for x,player in df_season.iterrows():
     win_counter = 0
-    df_temp = pd.DataFrame(client.regular_season_player_box_scores(player_identifier=player['slug'],season_end_year=2020))
+    df_temp = pd.DataFrame(client.regular_season_player_box_scores(player_identifier=player['slug'],season_end_year=current_year))
 
     for index, outcome in df_temp.iterrows():
         if (outcome['outcome'].value) == "WIN":
             win_counter = win_counter +1
     
     df_regular_players = df_regular_players.append({'name':player['name'],'slug':player['slug'],'wins':win_counter, 'WS':player['win_shares']},ignore_index=True)
-    print(player['name'],'nickname is', player['slug'], 'had x wins', str(win_counter), player['win_shares'])
 
 
+df_regular_players['my_calc'] = df_regular_players['WS'] / df_regular_players['wins']
 
-print("THIS IS THE REGULAR PLAYERS")
+if 'James Harden' in df_regular_players['name'].values:
+    print('sdfsgdsfgdsfgdfsgsd')
+
 print(df_regular_players)
-
-
-print("THIS IS THE MVP PLAYERS")
 print(df)
+
+plt.scatter(df_regular_players['my_calc'],df_regular_players['name'],label="Players in 2019-2020 season")
+plt.scatter(df['my_calc'],df['Player'], label='Past MVPS')
+
+plt.legend(loc='upper right', prop={'size':10})
+plt.yticks([])
+plt.show()
